6083d9ff2563c47687948ac028eafd1b61c9eb4c
Derk Norton <derk.norton@gmail.com>
2019-07-16 09:21:35 -0600

    Fixed problems in EEPROM class.

--- libraries/HSM/EEPROM.cpp
+++ libraries/HSM/EEPROM.cpp
@@ -1,30 +1,21 @@
 #include "EEPROM.h"
 
-#define EEPROM_SIZE 100
 
-
-EEPROM::EEPROM() {
-    memory = new uint8_t[EEPROM_SIZE];
-    memset(memory, 0x00, EEPROM_SIZE);
+int EEPROMClass::length() {
+    return EEPROM_SIZE;
 }
 
-
-EEPROM::~EEPROM() {
-    delete [] memory;
-}
-
-
-uint8_t EEPROM::read(int index) {
+uint8_t EEPROMClass::read(int index) {
     return memory[index % EEPROM_SIZE];
 }
 
 
-void EEPROM::write(int index, uint8_t value) {
+void EEPROMClass::write(int index, uint8_t value) {
     memory[index % EEPROM_SIZE] = value;
 }
 
 
-void EEPROM::update(int index, uint8_t value) {
+void EEPROMClass::update(int index, uint8_t value) {
     if (memory[index % EEPROM_SIZE] != value) {
         memory[index % EEPROM_SIZE] = value;
     }
--- libraries/HSM/EEPROM.h
+++ libraries/HSM/EEPROM.h
@@ -1,19 +1,24 @@
 #ifndef EEPROM_h
 #define EEPROM_h
 
+#define EEPROM_SIZE 100
+
 #include <inttypes.h>
 
 
-class EEPROM final {
+class EEPROMClass final {
 
   public:
-    static uint8_t read(int index);
-    static void write(int index, uint8_t value);
-    static void update(int index, uint8_t value);
+    int length();
+    uint8_t read(int index);
+    void write(int index, uint8_t value);
+    void update(int index, uint8_t value);
 
   private:
-    static uint8_t* memory;
+    uint8_t memory[EEPROM_SIZE];
 
 };
+
+static EEPROMClass EEPROM;
 
 #endif
